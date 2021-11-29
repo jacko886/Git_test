bb7d01e7b3c1bfe5815e4fe66638ab3c2e91dfd5
jacko <1466841106@qq.com>
2021-11-29 20:37:41 +0800

    hot-fix first commit

--- src/main/java/com/jacko/GitTest.java
+++ src/main/java/com/jacko/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Jacko!!!");
         System.out.println("Welcome to my home!!!");
+        System.out.println("checkout hot-fix");
     }
 }
